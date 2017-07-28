bbdda8c053f5e9f981dcc506eae4c2709d181050
Steffani Gomez <steffani_gomez@brown.edu>
2017-07-27 23:11:56 -0400

    continuing work on the breweryDB R wrapper and pulling info from it

--- BreweryDBDataRetriever.r
+++ BreweryDBDataRetriever.r
@@ -1,11 +1,32 @@
-library(jsonlite) # for working with JSON data
-library(lubridate) # for working with dates
+#library(jsonlite) # for working with JSON data
+#library(lubridate) # for working with dates
 
+source("BreweryDBRWrapper.R")
 
 # my BreweryDB API key
 breweryDBKey <- "11b192faea1a549172fe2423db077bc5" 
 
-source("BreweryDBRWrapper.R")
-
-beers <- BreweryDB_beers(breweryDBKey)
+beers <- BreweryDB_endpoint(breweryDBKey, "beers", options = list(ids = c("z3VfEn")))
 print(beers)
+
+beerStylesRequest <- BreweryDB_endpoint(breweryDBKey, "styles")
+print(beerStylesRequest)
+beerStylesData <- content(beerStylesRequest)
+names(beerStylesData)
+names(beerStylesData$data[[1]])
+length(beerStylesData$data)
+beerStylesData$data[[1]]
+beerStylesData$data[[171]]
+beerStylesData$data[[1]]$description
+
+# going to use the different styles to request the beers, and use
+# the styles to map the beers to categories
+# when requesting the beers, request with brewery information
+
+# going to need a category to style, style to beer for sure
+# want to make dynamic so we don't save all the data, 
+# however for the purpose of this class we are going to make sure
+# we save all the ids to beers and breweries, styles, categories, 
+# and locations
+# then begin porting in data from RateBeer about the different kinds
+# of beers
--- BreweryDBRWrapper.R
+++ BreweryDBRWrapper.R
@@ -6,9 +6,14 @@ breweryDBKey <- ""
 # the base BreweryDB URL that queries will be appended to
 breweryDBBaseURL <- "http://api.brewerydb.com/v2/"
 
-# this function returns the beers endpointn of the API, using parameter
-# requests if necessary
-BreweryDB_beers <- function(api_key) {
-  print(api_key)
-  GET(paste(breweryDBBaseURL, "beers", sep=""), query = list(key = api_key, ids = c("z3VfEn")))
+# this function returns the beers endpoint of the API, using parameter
+# it needs name, abv, ibu, glasswareId, srmId, availableId, styleId or ids
+# without premium to get beers
+BreweryDB_endpoint <- function(api_key, endpoint, options = NULL) {
+  if (is.null(options)) {
+    GET(paste(breweryDBBaseURL, endpoint, sep = ""), query = list(key = api_key))
+  } else {
+    paramsRequest <- c(options, key = api_key)
+    GET(paste(breweryDBBaseURL, endpoint, sep = ""), query = paramsRequest)
+  }
 }
