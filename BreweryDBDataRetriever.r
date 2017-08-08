b460bb6d1c117bc3435d338047d6bb9c805264e1
Steff <steffanigomez323@gmail.com>
2017-08-08 09:17:48 -0400

    started exploratory analysis and explaining it in project03 deliverable

--- BreweryDBDataRetriever.r
+++ BreweryDBDataRetriever.r
@@ -3,6 +3,7 @@ library(tidyjson) # also for working with JSON data
 # ran into bugs in the package so had to ditch it and do it by hand
 library(tidyverse) # to transform and clean data
 library(tidyr) # for help with turning JSONs into tidy frames
+#library(moments) # for calculating skewness
 
 source("BreweryDBRWrapper.R")
 
@@ -79,7 +80,7 @@ beers <- NULL
 # in order to make more dynamic, could keep ttrack of how many observations we've seen and 
 # do the math to see if we should update (not a priority)
 
-beersRequestData <- BreweryDB_endpoint(breweryDBKey, "beers", options = list(p = as.character(1))) %>%
+beersRequestData <- BreweryDB_endpoint(breweryDBKey, "beers", options = list(p = as.character(1), withBreweries = "Y")) %>%
   content(as = "text", encoding = "UTF-8") %>% fromJSON(simplifyDataFrame = TRUE)
 beerNumPages <- beersRequestData$numberOfPages
 
@@ -92,9 +93,8 @@ for (i in 1:beerNumPages) {
   unfilteredBeerData$categoryId <- unfilteredBeerData$style$categoryId
   
   unfilteredBeerData$breweryId <- lapply(unfilteredBeerData$breweries, FUN = function(x) { paste(x$id, collapse = " ") })
-  
 
-  headerstoAdd <- setdiff(c("id", "name", "description", "abv", "ibu","styleId"), 
+  headerstoAdd <- setdiff(c("id", "name", "description", "abv", "ibu","styleId", "srmId"), 
                           names(unfilteredBeerData))
   if (!is_empty(headerstoAdd)) {
     for (colName in headerstoAdd) {
@@ -103,11 +103,11 @@ for (i in 1:beerNumPages) {
   }
   if (is.null(beers)) {
     beers <- unfilteredBeerData %>%
-      select(id, name, description, abv, ibu, styleId, categoryId, breweryId) %>%
+      select(id, name, description, abv, ibu, styleId, categoryId, breweryId, srmId) %>%
       as_tibble()
   } else {
     beers <- rbind(beers, unfilteredBeerData %>% 
-                     select(id, name, description, abv, ibu, styleId, categoryId, breweryId) %>%
+                     select(id, name, description, abv, ibu, styleId, categoryId, breweryId, srmId) %>%
                      as_tibble())
   }
 }
@@ -253,7 +253,7 @@ beerCategoriesStyles <- beerStyles %>% inner_join(beerCategories %>% rename(cate
 #is.na(beers$name) <- beers$name == "NULL"
 
 for (attr in names(beers)) {
-  is.na(beers[attr]) <- beers[attr] == "NULL"
+  as.na(beers[attr]) <- beers[attr] == "NULL"
 }
 
 for (attr in names(breweries)) {
@@ -270,11 +270,6 @@ breweries <- breweries %>%
   unnest(locationId)
 write_rds(breweries, breweriesFile)
 
-
-beers$abv <- as.numeric(beers$abv)
-beers
-beers %>% ggplot(aes(x = abv)) + geom_histogram()
-beers %>% filter(abv > 50)
 
 # some serious cleaning is needed, have to dump outliers and values that don't make sense
 # we can use initial graphs to show the outliers
@@ -295,3 +290,93 @@ breweriesandLocations <- breweries %>% rename(breweryId = id, breweryName = name
 beersBreweriesLocations <- beers %>% rename(beerId = id, beerName = name, 
                                 beerDescription = description) %>% 
   inner_join(breweriesandLocations, by = "breweryId")
+
+################# DATA CLEANING #########################
+
+beers <- read_rds(beersFile)
+
+# SERIOUS CLEANING AND FILTERING IS NEEDED
+beers$abv <- as.numeric(beers$abv)
+beers$ibu <- as.numeric(beers$ibu)
+
+# now that we have all of our data, we might want to take a look at the distribution of the most distinguishable
+# beer characteristics, abv (alcohol per beer volume, expressed as a percentagage out of 100) and ibu (international
+# bitterness unit value, which is a measure of how bitter the beer is)
+
+summary(beers$abv)
+
+# summary statistics of the abv distribution out of all the beers reveal that the maximum abv is 308. Since a percent
+# is out of 100, everything above 100 doesn't make sense and we can remove all the beers whose abv is above 100
+# since the credibility of that beer is now questionable
+
+beers %>% filter(abv > 100) %>% nrow()
+beers %>% filter(abv > 100)
+
+# there is only 1 beer whose abv is above 100, and we dispose of that observation
+beers <- beers %>% filter(id != "EHPIi4")
+
+# now we visualize the distribution to get a better picture
+summary(beers$abv)
+beers %>% ggplot(aes(abv)) + geom_histogram(binwidth=1)
+
+# if we wanted to see the distribution of location types among the dataset
+beersBreweriesLocations %>% ggplot(aes(x = locationTypeDisplay)) + geom_bar()
+#beersBreweriesLocations %>% ggplot(aes(abv)) + geom_histogram(binwidth = 1) + facet_wrap(~ countryIsoCode)
+
+# it seems that the majority of beer abv is between 0 and 20, so let's visualize that
+beers %>% ggplot(aes(abv)) + geom_histogram(binwidth = 1) + xlim(0, 20)
+
+# the distribution could be considered normal, with a skewness of 3.428445
+
+#skewness(beers$abv, na.rm = TRUE)
+
+# let's take a look at ibu
+
+summary(beers$ibu)
+
+beersBreweriesLocations %>% filter(ibu > 500) %>% select(breweryName, beerName, ibu, countryIsoCode)
+
+# there are a few outliers but after googling to make sure of their authenticity, no observations are removed
+# from the beers for wrong ibu range
+
+summary(beers$ibu) # have to look up ibu to see what is the possible range
+# parts per million usually no more than 120
+
+beers %>% ggplot(aes(x = ibu)) + geom_histogram(colour = "black", binwidth = 10)
+
+beers %>% filter(ibu > 110) %>% nrow
+
+# only 249 observations above 110, ibu doesn't normally go above 120 according to research, the tongue can distinguish
+# up to 110 ibus
+
+beers %>% filter(ibu <= 110) %>% ggplot(aes(ibu)) + geom_histogram(colour = "black", binwidth = 5)
+
+# definitely right skewed
+
+beers %>% ggplot(aes(styleId)) + geom_bar()
+
+# find the top 5 styles and their categories
+
+# abv and ibu
+
+beersBreweriesLocations %>% ggplot(aes(abv, y = ibu)) + geom_point()
+beersBreweriesLocations %>% filter(countryIsoCode == "GB") %>%
+  ggplot(aes(abv, ibu)) + geom_jitter()
+
+# displaying the range of abv and ibu for each beer style would be great
+
+# getting statistics per country & state would also be great
+
+# displaying states with the most breweries would be nice
+
+# could also do count of types of breweries to see which are more popular
+
+# we are interested in the spread of beers and breweries across the world, so how are beers and breweries related
+# to their locations? what are the top styles in each country? what about state? what are the top 5 postal codes, 
+# states, and countries in regards to number of different breweries and then number of different beers? are they 
+# different? what does that say about beers and breweries? which breweries have the most beers? which breweries have
+# the most locations? are they different lists? why might this be? 
+
+# visualizations of style and category information would be very useful 
+
+####################### END #############################
